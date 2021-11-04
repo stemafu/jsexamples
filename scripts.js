2be1b08b97ca725813ce2232fb05f0545a886e1a
Steven Fulakeza <steven.fulakeza@lehman.cuny.edu>
2021-11-04 16:30:50 -0400

    switch statements and break

---
+++ scripts.js
@@ -0,0 +1,50 @@
+console.log("Hello external JavaScript file ");
+
+let dayInNumber = 5;
+
+switch(dayInNumber){
+    case 1:
+        /* The code that should execute if
+        dayInNumber matches 1.
+        */
+       console.log("Sunday");
+       break;
+    case 2:
+        /* The code that should execute if
+        dayInNumber matches 2.
+        */
+        console.log("Monday");
+        break;
+    case 3:
+        /* The code that should execute if
+        dayInNumber matches 3.
+        */
+        console.log("Tuesday");
+        break;
+    case 4:
+        /* The code that should execute if
+        dayInNumber matches 4.
+        */
+        console.log("Wednesday");
+        break;
+    case 5:
+        /* The code that should execute if
+        dayInNumber matches 5.
+        */
+        console.log("Thursday");
+        break;
+    case 6:
+        /* The code that should execute if
+        dayInNumber matches 6.
+        */
+        console.log("Friday");
+        break;
+    case 7:
+        /* The code that should execute if
+        dayInNumber matches 7.
+        */
+        console.log("Saturday");
+        break;
+    default:
+        console.log("You provided a wrong number");
+}
