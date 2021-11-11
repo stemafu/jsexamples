814763721545f92157483adbbcd04ea2c9369620
Steven Fulakeza <steven.fulakeza@lehman.cuny.edu>
2021-11-11 16:33:14 -0500

    for loops and while loops

--- scripts.js
+++ scripts.js
@@ -48,3 +48,94 @@ switch(dayInNumber){
     default:
         console.log("You provided a wrong number");
 }
+
+/* Below we add an example of while loops */
+console.log("while loops examples");
+
+/* a loop that prints even number from 2 to 100000
+let count = 2;
+while(count <= 100000){
+    console.log(count);
+    count = count + 2;
+}
+*/
+
+/* a loop that prints odd number from 3 to 100000
+count = 3;
+while(count <= 100000){
+    console.log(count);
+    count = count + 2;
+}
+
+==
+*/
+
+count = 6;
+let factorial = 1;
+while(count > 0){
+    //console.log(count);
+    factorial = factorial * count;
+    count = count - 1;
+}
+
+console.log(factorial);
+
+console.log("for loops examples");
+let i;
+for(i = 1; i <= 4; i++){
+    console.log(i);
+}
+console.log("Done");
+
+/* Day 21
+
+Activity 1
+Write a for loop that prints all the even number from 2 to 100000.
+
+What will be the starting point of this loop: 2
+And we have to stop at 100000
+
+*/
+
+console.log("Printing even numbers:");
+for( i = 2; i <= 100000; i = i + 2){
+    console.log(i);
+}
+
+
+/* Day 21
+
+Activity 2
+Write a for loop that prints all the odd numbers from 3 to 100000.
+
+
+*/
+console.log("Printing odd numbers:");
+for( i = 3; i <= 100000; i = i + 2){
+    console.log(i);
+}
+
+/* Day 21
+
+Activity 3
+Write a for loop that prints all the odd numbers from 3 to 100000.
+Hint: use an if statement to decide if a number is odd or not odd
+
+
+
+*/
+
+
+for(i = 3; i<=100000; i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+
+
+
+/* How do you determine if a number is odd using an if statement
+console.log( 4 % 2 );
+*/
+
